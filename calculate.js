4a7b6536dce10c1846132dc6aa88889c61bbeb6a
Thanakrit-1 <guyzxzxas@gmail.com>
2026-06-28 17:32:34 +0700

    fix: re-submitted calcualtor API quiz

--- calculate.js
+++ calculate.js
@@ -6,38 +6,45 @@ app.use(cors())
 
 const Port = 3000
 
-function Cal(a, b, op) {
-    let numberA = Number (a);
-    let numberB = Number (b);
-    if (isNaN(numberA) || isNaN(numberB))
-        return {error : "invalid numbers"};
-    let result ;
-    switch (op) {
-        case "+" : result = numberA + numberB; break;
-        case "-" : result = numberA - numberB; break;
-        case "*" : result = numberA * numberB; break;
-        case "/" : result = numberA / numberB; break;
-        default: return {error : "invalid operator"};
+app.get('/add', (req, res) => {
+    let numA = Number(req.query.a);
+    let numB = Number(req.query.b);
+    if(isNaN(numA) || isNaN(numB)) {
+        return res.json({ error: "กรอกตัวเลขให้ถูก"});
     }
-    return result;
-}
-
-app.get('/calculate', (req, res) => {
-    let a = req.query.a;
-    let b = req.query.b;
-    let op = req.query.op;
-
-    let result = Cal(a, b, op);
-    if (result.error) {
-        return res.status(404).json({
-            "error": result.error
-        })
+    let result = numA + numB;
+    res.json({ Answer: result});
+});
+app.get('/subtract', (req, res) => {
+    let numA = Number(req.query.a);
+    let numB = Number(req.query.b);
+    if(isNaN(numA) || isNaN(numB)) {
+        return res.json({ error: "กรอกตัวเลขให้ถูก"});
     }
-
-    res.json({
-        "Answer": result
-    })
-})
+    let result = numA - numB;
+    res.json({ Answer: result});
+});
+app.get('/multiply', (req, res) => {
+    let numA = Number(req.query.a);
+    let numB = Number(req.query.b);
+    if(isNaN(numA) || isNaN(numB)) {
+        return res.json({ error: "กรอกตัวเลขให้ถูก"});
+    }
+    let result = numA * numB;
+    res.json({ Answer: result});
+});
+app.get('/divide', (req, res) => {
+    let numA = Number(req.query.a);
+    let numB = Number(req.query.b);
+    if(isNaN(numA) || isNaN(numB)) {
+        return res.json({ error: "กรอกตัวเลขให้ถูก"});
+    }
+    if (numB === 0) {
+        return res.json({ error: "ห้ามหารด้วย 0"});
+    }
+    let result = numA / numB;
+    res.json({ Answer: result});
+});
 
 app.listen(3000, () => {
     console.log("Server starts successfully")
--- calculate2.js
+++ calculate2.js
@@ -2,13 +2,15 @@ function calculate() {
     let num1 = document.getElementById('numberA').value;
     let num2 = document.getElementById('numberB').value;
     let operator = document.getElementById('op').value;
-
     let result_tag = document.getElementById("result");
-    if (operator === "/" &&  Number(num2) === 0) {
-        result_tag.innerText = "Result: Error";
-        return;
-    }
-    fetch(`http://localhost:3000/calculate?a=${num1}&b=${num2}&op=${encodeURIComponent(operator)}`)
+
+    let path = "";
+    if (operator === "+") path = "add";
+    else if (operator === "-") path = "subtract";
+    else if (operator === "*") path = "multiply";
+    else if (operator === "/") path = "divide";
+    
+    fetch("http://localhost:3000/" + path + "?a=" + num1 + "&b=" + num2)
         .then((res) => {
             return res.json()
         })
@@ -16,7 +18,7 @@ function calculate() {
         console.log(data)
             
         if (data.error) {
-            result_tag.innerText = "Result: Error - " + data.error;
+            result_tag.innerText = "Error: " + data.error;
 
         } else {
             result_tag.innerText = "Result: " + data.Answer;
